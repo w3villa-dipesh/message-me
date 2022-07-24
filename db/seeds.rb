9b1a7a3858e80ca63a2bb873ebb362c3ca921666
Dipesh Gupta <dipesh.gupta@w3villa.com>
2022-07-24 16:34:53 +0530

    Added user , message model and displayed original messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username:"dipeshgupta", password:"password1")
+User.create(username: 'sachin.s',password:"P@ssword2")
+User.create(username:"anujpal.78",password:"Password123")
+User.create(username:"shubham.raj01",password:"Password123")
+User.create(username:"rahul.jain",password:"P@sswOrd123")
+User.create(username:"dipeshh.guptaa",password:"P@sswOrd987")
+
