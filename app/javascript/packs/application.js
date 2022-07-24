64f7c8ce6feeda24dd6b07de19e35a7a02daa904
Dipesh Gupta <dipesh.gupta@w3villa.com>
2022-07-24 15:06:02 +0530

    Added dropdown navbar functionality

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,24 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from '@rails/ujs'
+import Turbolinks from 'turbolinks'
+import * as ActiveStorage from '@rails/activestorage'
+import 'channels'
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import '@doabit/semantic-ui-sass'
+
+// if you wan't to use custom variables, you should import custom styelesheet
+// import 'stylesheets/semantic-ui.scss'
+import '@doabit/semantic-ui-sass'
+$( document ).on('turbolinks:load', function() {
+  console.log("It works on each visit!")
+})
+
+$(document).on('turbolinks:load', function(){
+    $('.ui.dropdown').dropdown();
+})
